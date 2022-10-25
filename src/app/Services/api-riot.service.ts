2c892c1a5e19b9a0e61feb067e0fe9a1d417dc8a
Dass57 <116359487+Dass57@users.noreply.github.com>
2022-10-25 13:05:36 +0200

    [Add] api_key

--- src/app/Services/api-riot.service.ts
+++ src/app/Services/api-riot.service.ts
@@ -7,9 +7,10 @@ import { HttpClient } from '@angular/common/http';
 
 export class ApiRiotService {
   riot = 'https://euw1.api.riotgames.com'
+  api_key = 'RGAPI-944f6a5e-129c-46b9-a909-9532abda3844'
   constructor(private readonly httpClient: HttpClient) {}
 
   getFreeChampionRotation(){
-    return this.httpClient.get(`${this.riot}/lol/platform/v3/champion-rotations?api_key=RGAPI-126e3588-2434-47f3-95bf-814495333d8e`)
+    return this.httpClient.get(`${this.riot}/lol/platform/v3/champion-rotations?api_key=${this.api_key}`)
   }
 }
